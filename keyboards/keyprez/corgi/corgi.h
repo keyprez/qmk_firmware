6ab686901474a49198448637bd1e1d023b99d48f
csandven <chs_91@hotmail.com>
2021-03-20 19:56:43 +0100

    Keyprez Corgi keyboard

--- keyboards/keyprez/corgi/corgi.h
+++ keyboards/keyprez/corgi/corgi.h
@@ -37,10 +37,10 @@
     { k00, k01, k02, k03, k04, k05, KC_NO }, \
     { k10, k11, k12, k13, k14, k15, KC_NO }, \
     { k20, k21, k22, k23, k24, k25, KC_NO }, \
-    { k30, k31, k32, k39, k3a, k3b, r1t   }, \
+    { k30, k31, k32, k33, k34, k35, r1t   }, \
     { k06, k07, k08, k09, k0a, k0b, KC_NO }, \
     { k16, k17, k18, k19, k1a, k1b, KC_NO }, \
     { k26, k27, k28, k29, k2a, k2b, KC_NO }, \
-    { k36, k37, k38, k33, k34, k35, KC_NO } \
+    { k36, k37, k38, k39, k3a, k3b, KC_NO } \
 }
 
